3a54f02f69c522ba4f3b88fd025c0a3bb45c3ed6
dranhclub <58360612+dranhclub@users.noreply.github.com>
2021-01-02 23:40:06 +0700

    fix index

--- controllers/index-controller.js
+++ controllers/index-controller.js
@@ -1,16 +1,21 @@
 var ProductModel = require('../models/ProductModel');
+var Category = require('../models/CategoryModel');
 
 exports.index = async function(req, res, next) {
   var products = await ProductModel.findAll({limit: 20});
-  const MAX_LENGTH = 300;
-  for (i = 0; i < products.length; i++) { 
-    if (products[i].description.length > MAX_LENGTH) {
-      products[i].description = products[i].description.slice(0, MAX_LENGTH) + "...";
-    }
-  }
+  var products_1 = await (await Category.findOne({where: {name: 'Biography'}})).getProducts();
+  var products_2 = await (await Category.findOne({where: {name: 'History'}})).getProducts();
+  var products_3 = await (await Category.findOne({where: {name: "Children'"}})).getProducts();
+  var products_4 = await (await Category.findOne({where: {name: 'Art & Design'}})).getProducts();
+
   res.render('index', { 
     title: 'BookStore - Home Page', 
     products: products,
+    products_1,
+    products_2,
+    products_3,
+    products_4,
+    bestSeller: products,
     req
   });
 }
